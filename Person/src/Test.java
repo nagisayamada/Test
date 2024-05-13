2e88b8be9d294b3153bb0f52d866b5f912b75e9f
nagisayamada <nagisa0914@gmail.com>
2024-05-13 21:23:10 +0900

    robot

--- Person/src/Test.java
+++ Person/src/Test.java
@@ -1,13 +1,11 @@
 
 public class Test {
 	public static void main(String[] args){
-		Person taro = new Person();
+		Robot taro = new Robot();
 	
-	taro.name="山田太郎";
-	taro.age=20;
+		taro.name="aibo";
 	
 	System.out.println(taro.name);
-	System.out.println(taro.age);
 	
 	taro.talk();
 	taro.walk();
