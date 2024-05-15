b7cbb50c957b09119d1360886a9b8b2861f0a1bb
nagisayamada <nagisa0914@gmail.com>
2024-05-15 16:45:10 +0900

    person2

--- Person2/src/Person.java
+++ Person2/src/Person.java
@@ -9,5 +9,18 @@ public class Person {
 		this.name = name;
 		this.age = age;
 	}
+	public Person(String name) {
+		this.name = name;
+		this.age = 0;
+		
+	}
+	public Person(int age) {
+		this.name = "名前なし";
+		this.age = age;
+	}
+	public Person(int age,String name) {
+		this.name = name;
+		this.age = age;
+	}
 
 }
