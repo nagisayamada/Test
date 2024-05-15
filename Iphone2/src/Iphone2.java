9f18a48901a5f75e5246cc7eb926cb99f6cb8f33
nagisayamada <nagisa0914@gmail.com>
2024-05-15 19:01:17 +0900

    Iphone2

---
+++ Iphone2/src/Iphone2.java
@@ -0,0 +1,15 @@
+
+public class Iphone2 {
+	
+	public static void main(String[] args){
+		SmartPhone2 Iphone = new SmartPhone2();
+		Iphone.play();
+		Iphone.stop();
+		Iphone.next();
+		Iphone.back();
+		Iphone.call();
+		Iphone.mail();
+		Iphone.photo();
+		Iphone.internet();
+		}
+}
--- Iphone2/src/SmartPhone.java
+++ Iphone2/src/SmartPhone.java
@@ -13,5 +13,5 @@ public class SmartPhone implements Mp3Player{
 	public void back(){
 		System.out.println("戻る");
 		}
-
+	
 }
