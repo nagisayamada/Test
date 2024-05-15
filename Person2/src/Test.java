f35e48379428dbe8de8c147835a8a15fd41d0aae
nagisayamada <nagisa0914@gmail.com>
2024-05-15 16:52:50 +0900

    person2

--- Person2/src/Test.java
+++ Person2/src/Test.java
@@ -4,15 +4,18 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Person taro = new Person();
-		taro.name ="taro";
-		taro.age =18;
+		taro.name ="saburo";
+		taro.age =0;
 		System.out.println(taro.name);
 		System.out.println(taro.age);
 		
-		Person jiro = new Person("jiro",20);
+		Person jiro = new Person("名前なし",25);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
-
+		
+		Person hanako = new Person("hanako",17);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 	}
 
 }
