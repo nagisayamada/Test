4a8e4f1ec3dce776f376f059cfb8883128d89924
nagisayamada <nagisa0914@gmail.com>
2024-05-15 17:57:43 +0900

    capsule

--- Capsule/src/Person.java
+++ Capsule/src/Person.java
@@ -5,15 +5,19 @@ public class Person {
 	
 	public Person(String name,int age) {
 		this.name = name;
-		this.age = age;
+		this.age = 0;
 	}
 	public String getName() {
 		return this.name;
 	}
+	public int getAge() {
+		return this.age;
+	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	
-	
-
 }
